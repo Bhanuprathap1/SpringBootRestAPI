f8aa846a529613c27223ac50e5e41282c7ad3e32
Bhanuprathap1 <prathap_itguy@yahoo.com>
2019-11-15 19:30:07 -0500

    Update SpringBootProjTaskmgrApplication.java

--- src/main/java/com/org/fsd/proj/taskmgr/api/SpringBootProjTaskmgrApplication.java
+++ src/main/java/com/org/fsd/proj/taskmgr/api/SpringBootProjTaskmgrApplication.java
@@ -2,12 +2,15 @@ package com.org.fsd.proj.taskmgr.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootProjTaskmgrApplication {
+public class SpringBootProjTaskmgrApplication extends SpringBootServletInitializer {
+	/* SpringBootServletInitializer extended to generate the war file and deploy the application in external tomcat*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjTaskmgrApplication.class, args);
 	}
 
 }
+
